aa15ae330b1363646896fed6a6a8da4ea6fa4d4c
kevin <kevinfiorentino.1294@gmail.com>
2020-11-12 17:33:12 -0300

    modulo de pago, formulario terminado

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -6,6 +6,9 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 
 @NgModule({
@@ -15,12 +18,18 @@ import { MatDialogModule } from '@angular/material/dialog';
     FlexLayoutModule,
     MatButtonModule,
     MatCardModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [
     FlexLayoutModule,
     MatButtonModule,
     MatCardModule,
-    MatDialogModule
+    MatDialogModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class MaterialModule { }
